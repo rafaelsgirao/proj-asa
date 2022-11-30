9ba2387df99d547d6bf49c5b15adc104fb7082ab
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2022-11-30 16:24:49 +0000

    run make clean

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 using namespace std;
 #include <iostream>
 
-//Read matrix size
-int readSize(int &lines, int &cols)
-{
+// Read matrix size
+int readSize(int &lines, int &cols) {
     cin >> lines;
     cin >> cols;
     return 0;
@@ -12,7 +11,7 @@ int readSize(int &lines, int &cols)
 int main() {
     int lines;
     int cols;
-    readSize(lines,cols);
+    readSize(lines, cols);
     int matrix[lines][cols] = {0};
     cout << "The matrix is: " << endl;
     for (int i = 0; i < lines; i++) {
@@ -20,24 +19,23 @@ int main() {
             matrix[i][j] = 1;
         }
     }
-    //Print the matrix
+    // Print the matrix
     for (int i = 0; i < lines; i++) {
         for (int j = 0; j < cols; j++) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
-    //Loop through columns
+    // Loop through columns
     int col_caminho = 0;
     for (int cur_line = 0; cur_line < lines; cur_line++) {
 
         cin >> col_caminho;
-        for(int a=0; a < col_caminho; a++){
-            matrix[cur_line][a]=0;
-
+        for (int a = 0; a < col_caminho; a++) {
+            matrix[cur_line][a] = 0;
         }
     }
-    //Print the new matrix
+    // Print the new matrix
     cout << "The new matrix is: " << endl;
     for (int i = 0; i < lines; i++) {
         for (int j = 0; j < cols; j++) {
