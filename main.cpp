05bb098dd2872765804488e475859d84e28fac99
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2022-12-12 19:30:18 +0000

    Make fmt + we lost af

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 using namespace std;
 
+#include "main.h"
 #include <algorithm>
 #include <iostream>
 #include <vector>
-#include "main.h"
 
-// Global variables.
-int largest_square = 0;
 vector<int> path;
+uint numberOfCases = 0;
+
 Matrix matrix;
-uint numberOfCases =0;
-
-
 
 // Read matrix size
 int readSize(uint &lines, uint &cols) {
@@ -20,13 +17,22 @@ int readSize(uint &lines, uint &cols) {
     return 0;
 }
 
+void printMatrix() {
+    // Print the new matrix
+    cout << "DEBUG: The new matrix is: " << endl;
+    for (Line line : matrix) {
+        for (int sq : line) {
+            cout << sq << " ";
+        }
+        cout << endl;
+    }
+}
 
-
-
-Matrix initMatrix(int lines, uint cols) {
-    // Create a matrix of size lines x cols, initialized w/ empty squares
-    // https://stackoverflow.com/questions/65646403/initialization-of-vector-of-vectors
-    Matrix matrix(lines, vector<int>(cols, 1));
+void initMatrix(int lines, uint cols) {
+    for (int i = 0; i < lines; i++) {
+        Line newLine(cols, 1);
+        matrix.push_back(newLine);
+    }
 
     // Loop through columns
     int col_caminho = 0;
@@ -38,27 +44,56 @@ Matrix initMatrix(int lines, uint cols) {
             matrix[cur_line][a] = 0;
         }
     }
-    // Print the new matrix
-    cout << "DEBUG: The new matrix is: " << endl;
-    for (Line line : matrix) {
-        for (int sq : line) {
-            cout << sq << " ";
-        }
-        cout << endl;
-    }
-    return matrix;
+    printMatrix();
 }
 
-void removeSquare(int l,int c,int size){
-    for(int i=0;i<size;i++){
-        matrix[l][c+i]=0;
-        matrix[l+i][c]=0;
+void setSquare(int l, int c, int size, int color) {
+    for (int i = l; i < l + size; i++) {
+        for (int j = c; j < c + size; j++) {
+            matrix[i].at(j) = 1;
+        }
     }
-    
-
     return;
 }
 
+void addSquare(int l, int c, int size) { setSquare(l, c, size, 1); }
+void delSquare(int l, int c, int size) { setSquare(l, c, size, 0); }
+
+bool canAddSquare(int line, int cols, int size) {
+    for (int i = line; i < size + line; i++) {
+        for (int j = cols; j < size + cols; j++) {
+            if (matrix[i][j] == 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// f(0, 0, 1)
+
+int f(int line, int col, int size) {
+    int rslt = 0;
+    while (rslt == 0) {
+        if (canAddSquare(line, col, size)) {
+            addSquare(line, col, size);
+            f(line + size, col + size, size);
+        }
+    }
+
+    return rslt;
+}
+
+void calculatePossibilities(int c, int l) {
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j++; j++) {
+            if (matrix[i][j] == 0) {
+                setSquare(i, j, 1, 1);
+            }
+        }
+    }
+    return;
+}
 int main() {
     uint lines;
     uint cols;
@@ -66,9 +101,10 @@ int main() {
     // Read size of matrix dimensions
     readSize(lines, cols);
 
-
     // Create matrix
-    Matrix matrix = initMatrix(lines, cols);
-
+    initMatrix(lines, cols);
+    printMatrix();
+    f(0, 0, 1);
+    cout << canAddSquare(0, 0, 1);
     return 0;
 }
