e8403c32170dfd45176ec3f402ede839304dc7f4
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2022-12-12 18:33:55 +0000

    New start

--- main.h
+++ main.h
@@ -2,28 +2,6 @@
 #include <iostream>
 #include <vector>
 
-#define Line vector<Square *>
+#define Line vector<int>
 #define Matrix vector<Line>
 
-//#define uint unsigned int
-
-
-// Structs.
-struct square {
-    int size;     // Size of square we're building (or have built)
-    int cur_line; // Current line
-};
-typedef struct square Square;
-
-struct node {
-    uint line_nr;
-    struct node *parent;
-    Line prevLine;
-    vector<struct node *> children;
-};
-
-typedef struct node Node;
-
-// Function definitions.
-
-void getPossibleStates(Node *node, Line line) ;
